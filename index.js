ef992a219aaa90ffb1213105f21022314ef844fb
bar rot <151166429+barrot9@users.noreply.github.com>
2024-04-22 11:10:15 +0300

    init-commit

---
+++ index.js
@@ -0,0 +1,47 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+
+const app = express();
+const port = 3000;
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "permalist",
+  password: "a123456",
+  port: 5432,
+});
+db.connect();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+let items = [];
+
+app.get("/", async (req, res) => {
+  try {
+    const result = await db.query("SELECT * FROM items ORDER BY id ASC");
+    items = result.rows;
+    res.render("index.ejs", {
+      listTitle: "Today",
+      listItems: items,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+app.post("/add", (req, res) => {
+  const item = req.body.newItem;
+  items.push({ title: item });
+  res.redirect("/");
+});
+
+app.post("/edit", (req, res) => {});
+
+app.post("/delete", (req, res) => {});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
